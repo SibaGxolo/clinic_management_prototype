8a17e7fb1095c646b61acbd37248a19f3d56dce9
AsiveMrwe <67470316+AsiveMrwe@users.noreply.github.com>
2021-11-10 18:51:00 +0200

    Karabo's first commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { //karabo
     return const MaterialApp(
       home: LoginPage(),
     );
